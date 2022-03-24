d5a39de92883341d62ce03520264704ec5f114a0
Kai En Yong <kaien.yong99@gmail.com>
2022-03-24 21:18:46 +0800

    Added files for Week 10 Lab

---
+++ Question2.py
@@ -0,0 +1,4 @@
+x = input("Please enter a number: ")
+y = input("Please enter another number: ")
+average = (int(x) + int(y))/2
+print("The average of the two numbers are: " + str(average))
---
+++ Question3.py
@@ -0,0 +1,14 @@
+module_name = "CSC1009"
+
+switch = {
+    "CSC1006": "Mathematics 2",
+    "CSC1007": "Operating Systems",
+    "CSC1008": "Data Structures and Algorithms",
+    "CSC1009": "Object-Oriented Programming",
+    "CSC1010": "Computer Networks"
+}
+
+print(switch.get(module_name))
+
+for i in range(101, 65, -2):
+    print(i)
